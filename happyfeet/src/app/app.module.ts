ac41e049a49a872780fd2e3fbe3163f4b7f62796
SravyaVannemreddy <143159657+SravyaVannemreddy@users.noreply.github.com>
2024-02-21 23:33:25 -0600

    Sravya Vannemreddy Team 04 - 02-21-2024

--- happyfeet/src/app/app.module.ts
+++ happyfeet/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MainComponent } from './home/main/main.component';
+import { ProductsComponent } from './home/products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MainComponent
+    MainComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
