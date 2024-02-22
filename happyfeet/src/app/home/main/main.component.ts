d8e18f194c4b504541c5b6d69eb14ad89af24c76
Naresh <143207403+Chityala201@users.noreply.github.com>
2024-02-21 23:38:39 -0600

    Naresh 04 2/21/2024 ,Update on  main.component.ts

--- happyfeet/src/app/home/main/main.component.ts
+++ happyfeet/src/app/home/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-main',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+  c1data:any
+  currentSlide=0;
+  interval:any;
+
+  ngOnInit(){
+    this.c1data="https://www.google.com/url?sa=i&url=https%3A%2F%2Fstocksnap.io%2Fsearch%2Fshoes&psig=AOvVaw1y6ilXsSvd1XvQg2vcpoWh&ust=1708665223086000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCIii5I6YvoQDFQAAAAAdAAAAABAJ";
+
+  }
+
+  autoPlay(){
+    setInterval(()=>{this.nextSlide},2000)
+  }
+
+  nextSlide(){
+    this.currentSlide=(this.currentSlide+1) % this.c1data.length;
+  }
 
 }
