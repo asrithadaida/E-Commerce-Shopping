8bdf52ea3e0db4f3d58d03b09d7bb142fb0f2bed
asrithadaida <137849928+asrithadaida@users.noreply.github.com>
2024-02-26 23:10:54 -0600

    Asritha Daida - Team 04 - 02/26/2024

---
+++ happyfeet/src/Data/main.ts
@@ -0,0 +1,17 @@
+import path from "path";
+
+export const homeDate=[
+
+    {
+        Image: "https://www.google.com/imgres?imgurl=http%3A%2F%2Fbaccabucci.com%2Fcdn%2Fshop%2Fproducts%2F02_MG_8953_2.jpg%3Fv%3D1659946535&tbnid=DbIw3bg6uUg9HM&vet=12ahUKEwiOj-jS38qEAxXgyckDHbduBfAQMygNegUIARDzAg..i&imgrefurl=https%3A%2F%2Fbaccabucci.com%2Fproducts%2Fbacca-bucci-kids-unisex-high-top-fashions-sneakers-age-8-to-12-yrs&docid=A8OfIWoqyWg-oM&w=1800&h=1800&q=boys%20shoe%20image&ved=2ahUKEwiOj-jS38qEAxXgyckDHbduBfAQMygNegUIARDzAg",
+        path: "men/shoes"
+    },
+    {
+        Image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi5.walmartimages.com%2Fseo%2FFZM-Women-shoes-Ladies-Fashion-Solid-Color-Mesh-Metal-Chain-Decorative-Platform-Casual-Shoes_bfb2b081-c54d-468b-b044-1fba2bb4b5dc.f2c4d737b8c6eaa224af344f5a7cf3f9.jpeg%3FodnHeight%3D768%26odnWidth%3D768%26odnBg%3DFFFFFF&tbnid=l0nXUZn8nmVGgM&vet=12ahUKEwjw2o6K4MqEAxUGKtAFHZYuC_EQ94IIKAB6BQgBEOAB..i&imgrefurl=https%3A%2F%2Fwww.walmart.com%2Fip%2FFZM-Women-shoes-Ladies-Fashion-Solid-Color-Mesh-Metal-Chain-Decorative-Platform-Casual-Shoes%2F1357359533&docid=1JUCYGvQQFv8fM&w=768&h=768&q=women%20shoes&ved=2ahUKEwjw2o6K4MqEAxUGKtAFHZYuC_EQ94IIKAB6BQgBEOAB",
+        path: "Women/shoes"
+    },
+    {
+        Image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fboy-shoes&psig=AOvVaw1aCai2kPxpt6Zfn3agC9fW&ust=1709096741988000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCIjSp9PfyoQDFQAAAAAdAAAAABAE",
+        path: "kids/shoes"
+    }
+]
