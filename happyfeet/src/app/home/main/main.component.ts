75f8f84d39e6f7ad7c56ef834512f624db9e5515
kavyasree05 <137372024+kavyasree05@users.noreply.github.com>
2024-02-28 19:28:40 -0600

    Kavya Sree Konda   Team - 04   02-28-2024

--- happyfeet/src/app/home/main/main.component.ts
+++ happyfeet/src/app/home/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { interval } from 'rxjs';
+import { HomeComponent } from '../home.component';
 
 @Component({
   selector: 'app-main',
@@ -13,7 +14,12 @@ export class MainComponent {
 
   ngOnInit(){
     this.c1data="https://www.google.com/url?sa=i&url=https%3A%2F%2Fstocksnap.io%2Fsearch%2Fshoes&psig=AOvVaw1y6ilXsSvd1XvQg2vcpoWh&ust=1708665223086000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCIii5I6YvoQDFQAAAAAdAAAAABAJ";
+    this.autoPlay()
+  }
 
+  ngOnInit(){
+    this.c1data=HomeComponent;
+    this.autoPlay()
   }
 
   autoPlay(){
