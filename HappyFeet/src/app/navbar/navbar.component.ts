5299add693996fb5470c59f1557a2a954318f5f7
asrithadaida <137849928+asrithadaida@users.noreply.github.com>
2024-03-01 22:35:05 -0600

    Asritha Daida Team 04 - 03-01-2024

---
+++ HappyFeet/src/app/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import {component} from 'angular/core';
+
+@component({
+    selector:'app-navbar',
+    templateUrl:'./navbar.component.html',
+    styleUrls:['./navbar.component.scss']
+    })
+export class NavbarComponent {
+    openNavbarContent(menu:any){
+
+    }
+    navigateTo(path:any){
+
+    }
+}
