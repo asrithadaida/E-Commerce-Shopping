21bb9e85274bf48cdd38daa1ffa8421751d1accb
asrithadaida <137849928+asrithadaida@users.noreply.github.com>
2024-03-25 23:46:34 -0500

    Asritha Daida Team 04 - 03-25-2024

--- HappyFeet/src/app/navbar/nav-content.ts
+++ HappyFeet/src/app/navbar/nav-content.ts
@@ -13,7 +13,32 @@ export const navigation = {
             {name:'sneakers', id: 'sneakers'}
             {name:'formal shoes', id: 'formal shoes'}
             {name:'casuals', id: 'casuals'}
+            
+            ],
+        ]
+            
+        }
+        men:{
+            id:'men',
+            name:'men',
+            featured:[...
+            ],
+            sections:
+                {
+                id: 'shoes',
+                name: 'sandals',
+                items:[
+                {name:'sandals', id:'sandals'}
+                {name:'sneakers', id: 'sneakers'}
+                {name:'formal shoes', id: 'formal shoes'}
+                {name:'casuals', id: 'casuals'}
+                
+                ],
+            
+                
+            }
+
+
 
 
     }
-}
