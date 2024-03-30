3b9dc7ae06314652984581f94fa4b0229a1b8cfe
kavyasree05 <137372024+kavyasree05@users.noreply.github.com>
2024-03-29 23:06:35 -0500

    Kavya Sree Konda    Team - 04    03-29-2024

--- HappyFeet/src/app/Module/feature.module.ts
+++ HappyFeet/src/app/Module/feature.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from '../home/home.component';
+import { MainComponent } from '../home/main/main.component';
+import { ProductSliderComponent } from '../home/home-products/product-slider.component';
+import { HomeProductsComponent } from '../home/home-products/home-products.component';
 
 @NgModule({
     declarations: [
-        FeatureComponent
+        FeatureComponent,
+        HomeComponent,
+        MainComponent,
+        ProductSliderComponent,
+        HomeProductsComponent
     ],
     imports: [
         CommonModule
     ],
     exports: [
-        FeatureComponent
+        FeatureComponent,
+        HomeComponent
     ]
 
 })
