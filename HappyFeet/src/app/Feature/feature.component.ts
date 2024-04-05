13fc447a7f22a38dd2780fc09482c4ec69a0e65b
sai000teja007 <137807543+sai000teja007@users.noreply.github.com>
2024-04-05 09:39:48 -0500

    Sai Teja Thatikonda Team 04 - 04-05-2024

---
+++ HappyFeet/src/app/Feature/feature.component.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@andular/common';
+
+@NgModule({
+    declarations:[],
+    imports:[
+        CommonModule
+    ]
+})
+export class FeatureModule{ }
