5adbc18434961470f62c226b4557dc4f9ac95f43
SravyaVannemreddy <143159657+SravyaVannemreddy@users.noreply.github.com>
2024-04-08 16:40:11 -0500

    general changes

--- HappyFeet/src/app/Feature/feature.component.ts
+++ HappyFeet/src/app/Feature/feature.component.ts
@@ -1,8 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@andular/common';
+import { HomeComponent } from '../home/home.component';
+import { ProductSliderComponent } from '../home/home-products/product-slider.component';
 
 @NgModule({
     declarations:[],
+<<<<<<< Updated upstream
+=======
+    imports:[
+       
+    ]
+>>>>>>> Stashed changes
     exports:[
         FeatureModule,
     ]
