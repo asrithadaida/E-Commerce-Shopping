cb6f9ee4a8414ac7717f88d883022ba171b96a6d
SravyaVannemreddy <143159657+SravyaVannemreddy@users.noreply.github.com>
2024-04-10 13:11:26 -0500

    Sravya Vannemreddy Team 04 - 04-10-2024

---
+++ HappyFeet/src/app/home/products/filtersData.ts
@@ -0,0 +1,13 @@
+export const filters = [
+    {
+        id:"color",
+        name:"color",
+        options: [
+            {value:"white", label:"white"}
+            {value:"beige", label:"beige"}
+            {value:"Blue", label:"blue"}
+           
+
+        ],
+    },
+]
