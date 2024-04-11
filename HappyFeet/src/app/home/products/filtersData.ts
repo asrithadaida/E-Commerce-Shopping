53930ea4b3db19b6d0472659ad9ad86bd144921d
sai000teja007 <137807543+sai000teja007@users.noreply.github.com>
2024-04-10 21:28:30 -0500

    Sai Teja Thatikonda - Team04- 04/10/2024

--- HappyFeet/src/app/home/products/filtersData.ts
+++ HappyFeet/src/app/home/products/filtersData.ts
@@ -6,6 +6,11 @@ export const filters = [
             {value:"white", label:"white"}
             {value:"beige", label:"beige"}
             {value:"Blue", label:"blue"}
+            {value:"Brown", label:"Brown"}
+            {value:"green", label:"green"}
+            {value:"purple", label:"purple"}
+            {value:"yellow", label:"yellow"}
+            {value:"black", label:"black"}
            
 
         ],
